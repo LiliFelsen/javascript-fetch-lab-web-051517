3df41f7451fcdf3c57bd86e863b5a567adb3b3e8
Lili Ouaknin <aurelie.ouaknin@gmail.com>
2017-07-06 16:14:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,60 @@
+const TOKEN = '7acfb41840cba0fd10c38219837a57ccde2ad98b'
+const userName = 'LiliFelsen'
+const baseApi = 'https://api.github.com/'
+const fork = `${userName}/javascript-fetch-lab`
+const repoURL = `${baseApi}repos/${fork}/forks`
+const issueURL = `${baseApi}repos/${fork}/issues`
+
 function getIssues() {
+  fetch(issueURL, {
+    method: 'GET',
+    headers: {
+      Authorization: `token ${TOKEN}`
+    }
+  }).then(response => response.json())
+  .then(json => json.forEach(function(e) {
+  showIssues(e)}))
 }
 
-function showIssues(json) {
+function showIssues(data) {
+  console.log(data)
+ $('#issues').append(`<a href="${data.html_url}">${data.html_url}</a><br>`)
 }
 
 function createIssue() {
+  let title = $('#title').val()
+  let text = $('#text').val()
+  let issueData = {title: title, body: text}
+  fetch(issueURL, {
+    method: 'POST',
+    body: JSON.stringify(issueData),
+    headers: {
+      Authorization: `token ${TOKEN}`
+    }
+  }).then(response => response.json())
+  .then(json => getIssues())
 }
 
 function showResults(json) {
 }
 
+function showForkedRepo(data) {
+  console.log(data)
+ $('#results').append(`<a href="${data.svn_url}">${data.svn_url}</a>`)
+}
+
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
+  fetch(repoURL, {
+    method: 'POST',
+    headers: {
+      Authorization: `token ${TOKEN}`
+    }
+  }).then(response => response.json())
+  .then(json => showForkedRepo(json))
 }
+
 
 function getToken() {
   //change to your token to run in browser, but set
